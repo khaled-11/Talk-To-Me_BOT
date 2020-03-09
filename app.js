57843ccae26e4e95b737ac526eff7d734e5b7835
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-03-09 15:29:30 -0400

    Change Personas in .env

--- app.js
+++ app.js
@@ -203,10 +203,10 @@ app.get("/profile", (req, res) => {
           to your environment variables:</p>"
         );
         res.write("<ul>");
-        res.write(`<li>PERSONA_BILLING = ${config.personaBilling.id}</li>`);
-        res.write(`<li>PERSONA_CARE = ${config.personaCare.id}</li>`);
-        res.write(`<li>PERSONA_ORDER = ${config.personaOrder.id}</li>`);
-        res.write(`<li>PERSONA_SALES = ${config.personaSales.id}</li>`);
+        res.write(`<li>PERSONA_DONATIONS = ${config.personaDonations.id}</li>`);
+        res.write(`<li>PERSONA_OTHERS = ${config.personaOthers.id}</li>`);
+        res.write(`<li>PERSONA_ADVICE = ${config.personaAdvice.id}</li>`);
+        res.write(`<li>PERSONA_INNER = ${config.personaInner.id}</li>`);
         res.write("</ul>");
       }
       if (mode == "nlp" || mode == "all") {
